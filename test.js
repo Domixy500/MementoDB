774303fe638450fac182ebf2cb07b05cc9d8e5f4
Domi <Dominik@PC2013>
2022-03-12 10:19:18 +0100

    ref

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 function Reference(e) {
+	this.DisplayName = e.field("DisplayName");
+	this.Type = new ObjectType(e.field("Type"));
 	
+	this.Show = function() {
+		message("id:\n" + this.id + "\nDisplayName:" + this.DisplayName + "\nType:" + this.Type.Name);
+	}
 }
 
 function ObjectType(e) {
