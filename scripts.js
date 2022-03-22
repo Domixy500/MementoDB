74d1fbdd32c2616bf48027156b113cbb36949197
Domixy500 <95097682+Domixy500@users.noreply.github.com>
2022-03-22 21:47:22 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,4 +4,8 @@ function RunScript(scriptName) {
 
 function Script_InitializeDB() {
   message("Init");
+// BaseObjectType
+  var obj = Create("BaseObject");
+  var Id = obj.field("Id");
+  message(Id);
 }
